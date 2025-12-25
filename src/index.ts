8663ea6170807f3aae089fe1f66cbe8ed12903c0
Andrii Yakovenko <andrii.yakovenko@onlyplay.net>
2025-12-25 23:21:47 +0200

    tune 2

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ exefe(async (): Promise<void> => {
     // inside each group: base, 1.5×base, 2×base
     const groupMultipliers = [2, 3, 4] as const; // divide by 2 => 1×, 1.5×, 2×
 
-    for (let base = 8; base <= 2048; base *= 4) {
+    for (let base = 8; base <= 8192; base *= 4) {
         for (const m of groupMultipliers) {
             const n = (base * m) / 2;
             const color = m === 3 ? 0x7FC248 : 0xB7855F;
@@ -26,4 +26,14 @@ exefe(async (): Promise<void> => {
             });
         }
     }
+
+    updateRenderer({
+        sequence: generateCollatzSequenceAdic(32).sequence,
+        color: 0xff0000,
+    });
+
+    updateRenderer({
+        sequence: generateCollatzSequenceAdic(64).sequence,
+        color: 0xff0000,
+    });
 });
